48fe5d9236be44f35c9e4403da43539f8872d193
Tural <tural.isazada@gmail.com>
2025-05-07 21:51:42 +0200

    main modified

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,11 @@
-from fastapi import FastAPI, Request, Query
 from backend.whisper_utils import transcribe_youtube_video
-from backend.supabase_codes import save_to_supabase, get_transcript_data  # Note: file is supabase.py not supabase_codes.py
-from backend.llm_utils import generate_quiz
+from backend.supabase_codes import save_to_supabase, get_transcript_data  
+from backend.chat_ollama import generate_quiz
+from backend.chat_groq import generate_quiz_groq
+from backend.prompt_text import build_prompt
 
 if __name__ == "__main__":
     transcript = transcribe_youtube_video("https://www.youtube.com/watch?v=TUIAVnvujOo")
     save_to_supabase(transcript)
     data = get_transcript_data(transcript["filename"])
-    print(generate_quiz(data["transcript"]))
-
-
-
-
-# app = FastAPI()
-
-# @app.get("/generate-quiz/")
-# def generate_quiz_endpoint(url: str = Query(..., description="Youtube video URL")):
-#     transcript = transcribe_youtube_video(url)
-#     # print(transcript)
-#     try:
-#         save_to_supabase(transcript)
-#     except Exception as e:
-#         print(f"Error saving to Supabase: {e}")
-#     # print("did save")
-#     # get_transcript_data(transcript["filename"])
-#     # print("got data")
-#     quiz = generate_quiz(transcript["transcript"])
-#     return {"quiz": quiz}
+    print(generate_quiz_groq(build_prompt(data["transcript"])))
