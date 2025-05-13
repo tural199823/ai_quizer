8d9d953a29b2d7f266ba564cec6dc0f23c1d57be
Tural <tural.isazada@gmail.com>
2025-05-13 22:06:51 +0200

    changed fasstapi logic

--- backend/fasstapi.py
+++ backend/fasstapi.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI, Request, Query
-from whisper_utils import transcribe_youtube_video
+from whisper_utils import transcribe_youtube_video, get_name_of_file
 from supabase_codes import save_to_supabase, get_transcript_data  
-from chat_ollama import generate_quiz
 from chat_groq import generate_quiz_groq
-from prompt_text import build_prompt
 
 
 app = FastAPI()
 
 @app.get("/generate-quiz/")
 def generate_quiz_endpoint(url: str = Query(..., description="Youtube video URL")):
-    transcript = transcribe_youtube_video(url)
-    save_to_supabase(transcript)
-    data = get_transcript_data(transcript["filename"])
-    quiz = generate_quiz_groq(build_prompt(data["transcript"]))
-    return {"quiz": quiz}
+    try:
+        filename = get_name_of_file(url)
+        data = get_transcript_data(filename)
+        if data:
+            quiz = generate_quiz_groq(data["transcript"])
+            return {"quiz": quiz}
+        else:
+            pass
+    except ValueError as e:
+        # If the transcript is not found, transcribe the video and save it to Supabase  
+        transcript = transcribe_youtube_video(url)
+        save_to_supabase(transcript)
+        data = get_transcript_data(transcript["filename"])
+        quiz = generate_quiz_groq(data["transcript"])
+        return {"quiz": quiz}
