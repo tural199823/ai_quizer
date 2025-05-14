113a5ee54c361046e411a066b482aa6314490ae2
Tural <tural.isazada@gmail.com>
2025-05-14 22:16:54 +0200

    suapabase codes modified

--- backend/supabase_codes.py
+++ backend/supabase_codes.py
@@ -5,6 +5,9 @@ from pathlib import Path
 import os
 
 def get_supabase_client() -> Client:
+    """
+    Create a Supabase client using the URL and Key from the .env file.
+    """
     env_path = Path(r"C:\Users\tural\ai_quizer\backend\.env").parent / ".env"
     load_dotenv(dotenv_path=env_path)
 
@@ -17,6 +20,9 @@ def get_supabase_client() -> Client:
     return create_client(url, key)
 
 def save_to_supabase(data: dict) -> None:
+    """
+    Save the transcript data to the Supabase database. 
+    """
     supabase = get_supabase_client()
 
     to_upsert = {
@@ -25,9 +31,12 @@ def save_to_supabase(data: dict) -> None:
     }
 
     response = supabase.table("Transcripts").upsert(to_upsert).execute()
-    print("Saved to Supabase:", response)
+    print("Saved to Supabase")
 
 def get_transcript_data(video_filename: str):
+    """
+    Retrieve the transcript data from the Supabase database using the filename.
+    """
     supabase = get_supabase_client()
 
     response = supabase.table("Transcripts").select("filename, transcript").eq("filename", video_filename).execute()
@@ -41,12 +50,3 @@ def get_transcript_data(video_filename: str):
     else:
         raise ValueError(f"No transcript found for filename: {video_filename}")
 
-
-
-# env_path = Path(r"C:\Users\tural\ai_quizer\backend\.env").parent / ".env"
-# load_dotenv(dotenv_path=env_path)
-
-# url = os.getenv("SUPABASE_URL")
-# key = os.getenv("SUPABASE_KEY")
-
-# print(url, key)
